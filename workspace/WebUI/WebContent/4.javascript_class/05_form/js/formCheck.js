a7f65581b46fa81bfbd43fe961116e77f37af96c
vldzm1015 <vldzm1015@gmail.com>
2019-06-05 18:21:41 +0900

    no message

--- workspace/WebUI/WebContent/4.javascript_class/05_form/js/formCheck.js
+++ workspace/WebUI/WebContent/4.javascript_class/05_form/js/formCheck.js
@@ -1,8 +1,34 @@
 window.onload = function(){
 
-		var frm = document.getElementById('frm');
+		//var frm = document.getElementById('frm');
+		//var inputs = document.getElementsByTagName('input');
+		
+		var frm = document.querySelector('#frm');
 		var inputs = document.querySelectorAll("input");
 		
-
+//		frm.onsubmit = function(e){
+//			e.preventDefault();
+//			e.stopPropagation();
+		frm.btn.onclick = function(){
+			var agree = frm.agree;
+			//alert(agree.checked);
+			if(!agree.checked){
+				alert('반드시 확인하셔야 합니다.');
+				return;
+			}
+		//별칭이 ['babo','geni','idiot','gdb']
+			var nick = ['babo','geni','idiot','gdb'];
+			var nickname = frm.nickname;
+			if (!nick.includes(frm.nickname.value)) {
+				alert('nickname (babo, geni, idiot, gdb) 중에 골라봐');
+				return;
+			}
+			
+			
+			
+			
+			
+			frm.submit();
+		}
 
 	}
--- workspace/WebUI/WebContent/4.javascript_class/99_practice/2_menupan/script.js
+++ workspace/WebUI/WebContent/4.javascript_class/99_practice/2_menupan/script.js
@@ -7,10 +7,23 @@ window.onload = function (){
 	var price = new Array();
 	var content = new Array();
 
-	//#### 1. TD에 이벤트 발생
-	//#####  2. 확인 버튼 클릭시 선택한 TD의 색을 다시 흰색으로
-	//#####  3. 값 계산하여 총합 구하기 - 각각 TD 클릭시 배열에 저장하였다가 계산
+//	//#### 1. TD에 이벤트 발생
+//	//#####  2. 확인 버튼 클릭시 선택한 TD의 색을 다시 흰색으로
+//	//#####  3. 값 계산하여 총합 구하기 - 각각 TD 클릭시 배열에 저장하였다가 계산
 
-	
-	
+	var td = document.getElementsByClassName('menu');
+	var option = document.getElementById('count');
+//	alert(option.length);
+
+	for (var i = 0; i < td.length; i++) {
+		td[i].onclick = function(){
+			price.push(this.childNodes[0].nodeValue+ 
+					this.childNodes[2].nodeValue);
+		}
+	}
+	function option(){
+		price.push(this.childNodes[0].nodeValue);
+		alert(price);
+	}
+
 }
